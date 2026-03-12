f200e31aa98f8731116747f12c132d29009c3e2c
sheetam522 <sheetambiswal@gmail.com>
2026-03-12 15:25:06 +0530

    Initial commit: Book My Stay App

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking Management System ");
+        System.out.println(" Version 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
