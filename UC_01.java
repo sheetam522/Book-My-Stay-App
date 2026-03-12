3f46c7411390e28bd7a244c893f2926e10c799f1
sheetam522 <sheetambiswal@gmail.com>
2026-03-12 22:12:29 +0530

    Initial commit

---
+++ UC_01.java
@@ -0,0 +1,14 @@
+public class UC_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("     Welcome to Book My Stay App     ");
+        System.out.println("   Hotel Booking Management System   ");
+        System.out.println("            Version 1.0              ");
+        System.out.println("=====================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay App.");
+    }
+}
